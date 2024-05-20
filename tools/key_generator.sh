d35a2f3b56b180cfbf63030592a9e7f6892d0144
01eguer <oleguerserrat@proton.me>
2024-05-20 23:39:54 +0200

    new key_generator.sh

--- tools/key_generator.sh
+++ tools/key_generator.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
-# Check if the length of the key is provided as a parameter
-if [ "$#" -ne 1 ]; then
-	echo "Usage: $0 <key_length>"
-	exit 1
-fi
+# Define the number of bytes in the key
+key_len=1024
 
-# Generate binary file with random data
-head -c "$1" /dev/urandom > key.bin
+# Generate random bytes for the key
+random_key=$(openssl rand -hex $((key_len/2)))
+
+# Print the key in the desired format
+echo "unsigned char key[] = {"
+count=0
+for ((i=0; i<${#random_key}; i+=2)); do
+    printf "  0x%s%s" "${random_key:$i:2}"
+    if [ $((i+2)) -lt ${#random_key} ]; then
+        echo -n ","
+    fi
+    ((count++))
+    if [ $count -eq 12 ]; then
+        echo
+        count=0
+    fi
+done
+echo "};"
+echo "unsigned int key_len = $key_len;"
 
